1e76607140d0b16f893b6ad2cc787c5665fff036
Isla <islarcherjr@gmail.com>
2025-03-18 09:57:56 +0100

    my first commit.

---
+++ src/test/java/org/project/bloggingplatform/BloggingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.bloggingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
